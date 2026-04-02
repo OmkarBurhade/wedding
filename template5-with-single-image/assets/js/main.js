740a3adff7a01f5c5ddae335e7b167d1ced1d9e9
Omkar burhade <burhadeomkar0603@gmail.com>
2026-04-02 14:23:53 +0530

    all new chnages updated

--- template5-with-single-image/assets/js/main.js
+++ template5-with-single-image/assets/js/main.js
@@ -1,25 +1,23 @@
+import { initAudioPlayer } from "./audio-player.js";
 import { initCountdown } from "./countdown.js";
 import { gallery } from "./gallery.js";
 import { initInvitation } from "./invitation.js";
 import { initLangToggle } from "./lang-toggle.js";
 // import { initLogoLoader } from "./loader.js";
 import { initMapToggles } from "./map-toggle.js";
-import { initMusicPlayer } from "./music.js";
 import { initRsvpForm } from "./rsvp-form.js";
 import { theme } from "./theme-2.js";
-import { videoPopup } from "./video-popup.js";
 import { initWishesForm } from "./wishes-form.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   // initLogoLoader();
+  initAudioPlayer();
   gallery();
   initInvitation();
-  initMusicPlayer();
   initCountdown();
   initMapToggles();
   initRsvpForm();
   initWishesForm();
   initLangToggle();
-  videoPopup();
-  theme()
+  theme();
 });
